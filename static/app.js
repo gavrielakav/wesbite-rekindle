f0bddaa801fb633a5873e30dbfeb6671d1fea216
Kavatsmara Fayra <126572167+gavrielakav@users.noreply.github.com>
2024-08-05 15:30:50 +0700

    Update app.js

--- static/app.js
+++ static/app.js
@@ -53,7 +53,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 };
         this.messages.push(msg1);
 
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('https://wesbite-rekindle.vercel.app/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
